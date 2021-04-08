ab0d9e019ed5f122d138a8deaa4ee9e1ed856dcd
Daichi Kanke <kanke@tam-tam.co.jp>
2021-04-09 02:55:49 +0900

    answer 14

--- 14.longest-common-prefix.ts
+++ 14.longest-common-prefix.ts
@@ -7,50 +7,32 @@
 // @lc code=start
 function longestCommonPrefix(strs: string[]): string {
     if (strs.length === 0) return ''
-
     let baseStr = strs[0]
     for (let i = 1; i < strs.length; i++) {
         if (baseStr.length > strs[i].length) baseStr = strs[i]
     }
     const baseStrArr = baseStr.split('')
+    const patterns: string[] = []
+    let tempCommonPrefixs: string[] = []
+    for (let i = 0; i < baseStrArr.length; i++) {
+        const str = i === 0 ? baseStrArr[i] : patterns[i - 1] + baseStrArr[i]
+        patterns.push(str)
+        tempCommonPrefixs.push(str)
+    }
 
     strs.splice(strs.indexOf(baseStr), 1)
 
-    let tempCommonPrefix: string[] = []
-
-    for (let strsIndex = 0; strsIndex < strs.length; strsIndex++) {
-        const commonPrefixArr = []
-        for (let baseStrPosition = 0; baseStrPosition < baseStrArr.length; baseStrPosition++) {
-            if (strs[strsIndex].indexOf(baseStrArr[baseStrPosition]) > -1) {
-                let commonPrefix = baseStrArr[baseStrPosition]
-
-                commonPrefixArr.push(commonPrefix)
-                for (let nextStr = baseStrPosition + 1; nextStr < baseStrArr.length; nextStr++) {
-                    if(strs[strsIndex].indexOf((commonPrefix + baseStrArr[nextStr])) > -1) {
-                        commonPrefix = commonPrefix + baseStrArr[nextStr]
-                        commonPrefixArr.push(commonPrefix)
-                    } else {
-                        break
-                    }
-                }
-            }
-        }
-
-        if (tempCommonPrefix.length === 0) {
-            tempCommonPrefix = commonPrefixArr
-        } else {
-            tempCommonPrefix = commonPrefixArr.filter(str => {
-                return tempCommonPrefix.includes(str)
-            })
-        }
+    for (let sIndex = 0; sIndex < strs.length; sIndex++) {
+        tempCommonPrefixs = tempCommonPrefixs.filter(str => {
+            return strs[sIndex].indexOf(str) === 0
+        })
+        if (tempCommonPrefixs.length === 0) return ''
     }
 
-    if (tempCommonPrefix.length === 0) return ''
-
     let result = ''
-    for (let i = 0; i < tempCommonPrefix.length; i++) {
-        if (tempCommonPrefix[i].length > result.length) {
-            result = tempCommonPrefix[i]
+    for (let i = 0; i < tempCommonPrefixs.length; i++) {
+        if (tempCommonPrefixs[i].length > result.length) {
+            result = tempCommonPrefixs[i]
         }
     }
     return result
