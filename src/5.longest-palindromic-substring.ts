6a4e073c8554a1aeb5c71986d766293ee52c43e3
Daichi Kanke <kanke@tam-tam.co.jp>
2021-04-28 21:48:59 +0900

    Answer 5

--- src/5.longest-palindromic-substring.ts
+++ src/5.longest-palindromic-substring.ts
@@ -6,8 +6,34 @@
 
 // @lc code=start
 function longestPalindrome(s: string): string {
-    return ''
+    if (s === '') return ''
+
+    const sArr = s.split('')
+    let result = sArr[0]
+
+    for (let i = 0; i < sArr.length; i++) {
+        if (result.length > sArr.length - i) break
+
+        const str = s.substr(i, s.length - i)
+
+        for (let j = 0; j < sArr.length; j++) {
+            const cropedStr = str.substr(0, str.length - j)
+
+            if (str[0] !== cropedStr.substr(-1, 1)) continue
+
+            if (cropedStr === cropedStr.split('').reverse().join('')) {
+                if (cropedStr.length > result.length) {
+                    result = cropedStr
+                }
+                break
+            }
+        }
+    }
+
+    return result
 };
 // @lc code=end
+
+// console.log(longestPalindrome('cbbdnclkwdnfk'))
 
 export { longestPalindrome }
