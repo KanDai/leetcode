56d07306440666c18f0f431e89cdef690dfbea18
Daichi Kanke <kanke@tam-tam.co.jp>
2021-05-05 12:21:11 +0900

    Answer 69

--- test/66.test.ts
+++ test/66.test.ts
@@ -1,29 +1,29 @@
 import { plusOne } from '../src/66.plus-one'
 
-test('3: case 1', () => {
+test('66: case 1', () => {
     expect(plusOne([1,2,3])).toEqual(expect.arrayContaining([1,2,4]));
 })
 
-test('3: case 2', () => {
+test('66: case 2', () => {
     expect(plusOne([4,3,2,1])).toEqual(expect.arrayContaining([4,3,2,2]));
 })
 
-test('3: case 3', () => {
+test('66: case 3', () => {
     expect(plusOne([5,6,7,8,9])).toEqual(expect.arrayContaining([5,6,7,9,0]));
 })
 
-test('3: case 4', () => {
+test('66: case 4', () => {
     expect(plusOne([0])).toEqual(expect.arrayContaining([1]));
 })
 
-test('3: case 5', () => {
+test('66: case 5', () => {
     expect(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3])).toEqual(expect.arrayContaining([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]));
 })
 
-test('3: case 6', () => {
+test('66: case 6', () => {
     expect(plusOne([9])).toEqual(expect.arrayContaining([1,0]));
 })
 
-test('3: case 7', () => {
+test('66: case 7', () => {
     expect(plusOne([9,9,9,9])).toEqual(expect.arrayContaining([1,0,0,0,0]));
 })
---
+++ test/69.test.ts
@@ -0,0 +1,13 @@
+import { mySqrt } from '../src/69.sqrt-x'
+
+test('69: Input 4', () => {
+    expect(mySqrt(4)).toBe(2)
+})
+
+test('69: Input 8', () => {
+    expect(mySqrt(8)).toBe(2)
+})
+
+test('69: Input 9', () => {
+    expect(mySqrt(9)).toBe(3)
+})
