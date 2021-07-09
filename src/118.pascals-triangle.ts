2c623070d88e17d218e356414d1c27657ec8c415
Daichi Kanke <KanDai@users.noreply.github.com>
2021-07-09 22:01:39 +0900

    Answer 118

--- src/118.pascals-triangle.ts
+++ src/118.pascals-triangle.ts
@@ -6,8 +6,29 @@
 
 // @lc code=start
 function generate(numRows: number): number[][] {
-    return [[1]]
+    let res = [[1]]
+    let i = 0
+
+    while (i < numRows - 1) {
+        const arr = []
+        for (let j = 0; j < res[i].length; j++) {
+            if (j === 0) {
+                arr.push(res[i][j])
+            } else {
+                arr.push(res[i][j] + res[i][j - 1])
+            }
+            if (j === res[i].length - 1) {
+                arr.push(res[i][j])
+            }
+        }
+        res.push(arr)
+        i++
+    }
+
+    return res
 };
 // @lc code=end
+
+// console.log(generate(5))
 
 export { generate }
